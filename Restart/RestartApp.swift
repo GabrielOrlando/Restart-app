ef9bf7a3183bf4887e3e10f019386fbfcee95575
Gabriel Orlando <gabriel.orlando00@gmail.com>
2024-06-06 20:47:36 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Gabriel Orlando on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
